f4405ecfc2baf1f91cc351672ab6e1fc25b13e6e
Demit <83365420+SasnyParen@users.noreply.github.com>
2023-11-21 01:33:19 +0300

    Json Add

--- src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,34 +1,14 @@
 package letscode.sarafan.controller;
 
-import letscode.sarafan.exceptions.NotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("exercise")
 public class MessageController {
-    private List<Map<String, String>> ExerciseList = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("ExtraInfo", "123123"); put("ExtraInfo2", "123123");}});
-    }};
-
-    @GetMapping
-    public List<Map<String, String>> list() {
-        return ExerciseList;
-    }
-
     @GetMapping("{id}")
-    public Map<String, String> getOne(@PathVariable String id) {
-        return getMessage(id);
-    }
-
-    private Map<String, String> getMessage(@PathVariable String id) {
-        return ExerciseList.stream()
-                .filter(message -> message.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
+    public String getOne(@PathVariable String id) throws IOException {
+        return ExerciseController.GetExercise(id);
     }
 }
